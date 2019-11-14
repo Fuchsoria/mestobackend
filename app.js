79a044329ea96279b4cf879385e51605993d90d6
German <sehenqq@gmail.com>
2019-11-14 17:59:32 +0100

    Add router replace

--- app.js
+++ app.js
@@ -1,5 +1,6 @@
 const express = require('express');
 const mongoose = require('mongoose');
+const bodyParser = require('body-parser');
 
 const { PORT = 3000 } = process.env;
 const app = express();
@@ -10,6 +11,13 @@ mongoose.connect('mongodb://localhost:27017/mestodb', {
   useFindAndModify: false,
 });
 
+app.use(bodyParser.json());
+app.use((req, res, next) => {
+  req.user = {
+    _id: '5dcb4ff4b1fa3610acf4dfd1',
+  };
+  next();
+});
 app.use(require('./routes'));
 
 app.listen(PORT, () => {
